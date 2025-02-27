4b32b22e7bc7c44a1ecfdcfe23f30243fcc24ff4
Strokin Constantine <strokinconstantine@gmail.com>
2025-02-27 22:25:33 +0300

    addition: optimization methods lab 1

--- small_projects/optimization_methods_labs/lab_1/gr.cpp
+++ small_projects/optimization_methods_labs/lab_1/gr.cpp
@@ -1,5 +1,42 @@
 #include <iostream>
 #include <cstdint>
+#include <cmath>
+
+
+
+double test_function_1( double x )
+{
+	return  x * ( x * ( x * ( x * ( -5 * x + 4 ) -12 ) + 11 ) -2 ) + 1;
+}
+
+double test_function_2( double x )
+{
+	return -( std::log( x - 2 ) * std::log( x - 2 ) ) + ( std::log( 10 - x ) * std::log( 10 - x ) ) - std::pow( x, 0.2 ); 
+}
+
+double test_function_3( double x )
+{
+	return -3 * x * std::sin( 0.75 * x ) + std::exp( -2.0 * x );
+}
+
+double test_function_4( double x )
+{
+	return std::exp( 3.0 * x ) + 5.0 * std::exp( -2.0 * x );
+}
+
+double test_function_5( double x )
+{
+	return 0.2 * x * std::log( x ) + ( x - 2.3 ) * ( x - 2.3 );
+}
+
+
+
+
+
+
+
+
+
 
 double gr( double a, double b, double (*f)( double ) )
 {
