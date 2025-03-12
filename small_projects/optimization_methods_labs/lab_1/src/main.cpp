03ebb982e00b10f0583b6228b00623d335b8b635
Strokin Constantine <strokinconstantine@gmail.com>
2025-03-12 22:14:19 +0300

    addition: optimization methods lab 1

--- small_projects/optimization_methods_labs/lab_1/src/main.cpp
+++ small_projects/optimization_methods_labs/lab_1/src/main.cpp
@@ -1,129 +1,160 @@
-#include <iostream>
-#include <cmath>
-#include <complex>
 #include <matplot/matplot.h>
 
-#include <periodic_absolute_value_function.h>
-#include <complex_function.h>
-#include <square_wave_function.h>
+//#include <periodic_absolute_value_function.h>
 
-void plot_f(
-	const frequency_methods_lab_1::function& f
-	, const std::string& plot_name
-	, double left_plot_argument_boundary
-	, double right_plot_argument_boundary
-	, uint_fast16_t plot_precision
-)
+#include <iostream>
+#include <cstdint>
+#include <cmath>
+#include <algorithm>
+
+
+double test_function_1( double x )
 {
-	std::vector<double> x = matplot::linspace( left_plot_argument_boundary, right_plot_argument_boundary, plot_precision );
+	return  x * ( x * ( x * ( x * ( -5 * x + 4 ) -12 ) + 11 ) -2 ) + 1;
+}
 
-	matplot::xlabel( "t" );
-    matplot::ylabel( plot_name );
-	
-	std::vector<double> actual_f = matplot::transform(
-		x
-		, [ &f ]( double t ) { return f.value( t ).real(); }
-	); 
-	std::vector<double> sum_of_fourier_series_of_f = matplot::transform(
-		x
-		, [ &f ]( double t ) { return f.sum_of_fourier_series( t ).real(); }
-	); 
-	std::vector<double> sum_of_fourier_series_in_complex_form_of_f = matplot::transform(
-		x
-		, [ &f ]( double t ) { return f.sum_of_fourier_series_in_complex_form( t ).real(); }
-	); 
-	
-	for ( size_t i = 0; i < plot_precision; ++i )
-	{
-	//	std::cout << actual_f[i] << "; " << sum_of_fourier_series_in_complex_form_of_f[i] << std::endl;
-	}
-	
-	
-	f.check_parsevals_identity(
-		-frequency_methods_lab_1::PI
-		, frequency_methods_lab_1::PI
-		, 0.001
-		, f.get_number_of_terms_in_fourier_series()
-	);
-	
-	matplot::plot( x, actual_f );
-	matplot::show();
-	matplot::cla(); 
-	
+double test_function_2( double x )
+{
+	return -( std::log( x - 2 ) * std::log( x - 2 ) ) + ( std::log( 10 - x ) * std::log( 10 - x ) ) - std::pow( x, 0.2 ); 
+}
 
-	matplot::plot( x, actual_f, x, sum_of_fourier_series_in_complex_form_of_f );
-	matplot::show();
-	matplot::cla();
-	
-	matplot::plot( x, actual_f, x, sum_of_fourier_series_of_f, x, sum_of_fourier_series_in_complex_form_of_f );
-	matplot::show();
-	matplot::cla();
+double test_function_3( double x )
+{
+	return -3 * x * std::sin( 0.75 * x ) + std::exp( -2.0 * x );
+}
 
-	
-	std::vector<double> difference_between_actual_f_and_sum_of_fourier_series_of_f( plot_precision );
-	std::vector<double> difference_between_actual_f_and_sum_of_fourier_series_in_complex_form( plot_precision );
-	double standard_deviation_for_sum_of_fourier_series_of_f = 0.0;
-	double standard_deviation_for_sum_of_fourier_series_in_complex_form_of_f = 0.0;
-	
-	for ( size_t i = 0; i < plot_precision; ++i )
-	{
-		difference_between_actual_f_and_sum_of_fourier_series_of_f[ i ] = actual_f[ i ] - sum_of_fourier_series_of_f[ i ];
-		difference_between_actual_f_and_sum_of_fourier_series_in_complex_form[ i ] = actual_f[ i ] - sum_of_fourier_series_in_complex_form_of_f[ i ];
-		standard_deviation_for_sum_of_fourier_series_of_f += ( difference_between_actual_f_and_sum_of_fourier_series_of_f[ i ] ) * ( difference_between_actual_f_and_sum_of_fourier_series_of_f[ i ] );
-		standard_deviation_for_sum_of_fourier_series_in_complex_form_of_f += ( difference_between_actual_f_and_sum_of_fourier_series_in_complex_form[ i ] ) * ( difference_between_actual_f_and_sum_of_fourier_series_in_complex_form[ i ] );
-	
-	}
-	standard_deviation_for_sum_of_fourier_series_of_f /= static_cast<double>( plot_precision );
-	standard_deviation_for_sum_of_fourier_series_in_complex_form_of_f /= static_cast<double>( plot_precision );
-	standard_deviation_for_sum_of_fourier_series_of_f = std::sqrt( standard_deviation_for_sum_of_fourier_series_of_f );
-	standard_deviation_for_sum_of_fourier_series_in_complex_form_of_f = std::sqrt( standard_deviation_for_sum_of_fourier_series_in_complex_form_of_f );
-	
-	
-	
-	matplot::ylabel( "Разница между исходной функцией и её рядом Фурье" );
-	matplot::plot( x, difference_between_actual_f_and_sum_of_fourier_series_of_f );
-	matplot::show();
-	
-	std::cout << "standard_deviation_for_sum_of_fourier_series_of_f: " << standard_deviation_for_sum_of_fourier_series_of_f << std::endl;
-	
-	matplot::cla();
+double test_function_4( double x )
+{
+	return std::exp( 3.0 * x ) + 5.0 * std::exp( -2.0 * x );
+}
 
-	matplot::plot( x, difference_between_actual_f_and_sum_of_fourier_series_in_complex_form );
-	matplot::show();
-	
-	std::cout << "standard_deviation_for_sum_of_fourier_series_in_complex_form_of_f: " << standard_deviation_for_sum_of_fourier_series_in_complex_form_of_f << std::endl;
-	
-	matplot::cla();
-	
-	
-	
+double test_function_5( double x )
+{
+	return 0.2 * x * std::log( x ) + ( x - 2.3 ) * ( x - 2.3 );
 }
 
 
 
 
-
-int main( void )
+double parabola(double a, double b, double (*f) (double), double epsilon, uint_fast32_t n)
 {
-	/*
-	plot_f(
-		frequency_methods_lab_1::complex_function( 50 )
-		, " |x| "
-		, -3.0 * matplot::pi
-		, 3.0 * matplot::pi
-		, 10'00
-	);
-	*/
+	double f_a = f(a);
+	double f_b = f(b);
 	
-	plot_f(
-		frequency_methods_lab_1::periodic_absolute_value( 5000 )
-		, " Модуль "
-		, -3.0 * matplot::pi
-		, 3.0 * matplot::pi
-		, 10'000
-	);
+	double c = (a + b) / 2;
+	
+	double f_c = f(c);
 
+	for (uint_fast32_t i = 1; i < n; ++i)
+	{
+		double u = c - ((c - a) * (c - a) * (f(c) - f(b)) - (c - b) * (c - b) * (f(c) - f(a)))
+			/ (2 * ((c - a) * (f(c) - f(b)) - (c - b) * (f(c) - f(a))));
+		if (c == b)
+			u = b;
+			
+	//	std::cout << "a: " << a << std::endl;
+		//std::cout << "c: " << c << std::endl;
+		//std::cout << "b: " << b << std::endl;
+		//std::cout << "u: " << u << std::endl;
+		double f_u = f(u);
+		if (f_u < f_c && u < c)
+		{
+			b = c;
+			f_b = f_c;
+			c = u;
+			f_c = f_u;
+		} else if (f_u > f_c && u < c)
+		{
+			a = u;
+			f_a = f_u;
+		} else if (f_u < f_c && u > c)
+		{
+			a = c;
+			f_a = f_c;
+			c = u;
+			f_c = f_u;
+		} else 
+		{
+			b = u;
+			f_b = f_u;
+		}
+		if (std::fabs(a - c) < epsilon)
+		{
+			return std::min(a, b);
+		}
+		
+		//std::cout << a << " " << b << std::endl;
+	}
+
+	return 0.0;
+}
+
+
+double gr( double a, double b, double (*f)( double ) )
+{
+	static const double k = 0.61803398875;
+	static uint16_t N = 0;
+	
+	while ( true )
+	{
+		double l = b - a;
+		double x = a + k * l; 
+		double y = b - k * l;
+		
+		std::cout << a << " " << b << std::endl;
+		
+		if ( f( x ) > f( y ) )
+			b = y;
+		else
+			a = x;
+		
+		N++;
+		if ( N > 100 )
+			return ( a + b ) / 2;
+	}
+	
+	return 0.0;
+}
+
+
+double f( double x )
+{
+	return x * x - 3 * x + 2;
+}
+
+int main()
+{
+	
+	// std::cout << gr( 1.0, 100.0, &f ) << std::endl;
 	
 	
-	return 0;
+	std::cout << parabola(1.0, 100.0, &f, 0.0001, 100) << std::endl;
+	
+	
+	double epsilon = 0.00001;
+	uint_fast32_t N = 100U;
+
+	std::vector<double> x = matplot::linspace(0.0,100.0, 1.0);
+	
+	
+	for (int i = 0; i < 100; ++i)
+	{
+		std::cout << x[i] << std::endl;
+	}
+	
+	
+	std::vector<double> err;
+	
+	err.reserve(N);	
+	
+	for (uint_fast32_t i = 0; i < N; ++i)
+	{
+		// parabola(1.0, 100.0, &f, epsilon, i);
+		
+		err[i] =  (parabola(1.0, 100.0, &f, epsilon, i) - 1.5);
+		std::cout << err[i];
+	}
+	
+	matplot::plot(x, err);
+	
 }
